723352c742f2e96bf2076af2657b299c5d37a6ca
Ljubixonovich <vikvikvuk@gmail.com>
2019-05-08 14:20:50 +0200

    keyboard is dissmised on click

--- src/screens/Login/Login.js
+++ src/screens/Login/Login.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, ImageBackground } from 'react-native';
+import { View, StyleSheet, ImageBackground, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import { connect } from 'react-redux';
 
 import { fonts } from '../../assets/index';
@@ -66,40 +66,44 @@ class LoginScreen extends Component {
          <ImageBackground source={require('../../assets/images/background.png')}
             style={{ flex: 1, width: '100%', height: '100%' }}
          >
-            <View style={styles.mainContainer}>
-               <View style={styles.inputContainer}>
-                  <DefaultInput
-                     placeholder='UserName' style={styles.input}
-                     value={this.state.controls.userName.value}
-                     valid={this.state.controls.userName.valid}
-                     touched={this.state.controls.userName.touched}
-                     onChangeText={(val) => this.updateInputState('userName', val)}
-                  />
-                  <DefaultInput
-                     placeholder='Password' style={styles.input}
-                     value={this.state.controls.password.value}
-                     valid={this.state.controls.password.valid}
-                     touched={this.state.controls.password.touched}
-                     onChangeText={(val) => this.updateInputState('password', val)}
-                     secureTextEntry
-                  />
-               </View>
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+               <View style={styles.mainContainer}>
 
-               <View style={styles.btnContainer}>
-                  <Btn
-                     width={200}
-                     textColor='black'
-                     color='#67B5CA'
-                     onPress={this.loginHandler}
-                     fontFamily={fonts.light}
-                     disabled={
-                        !this.state.controls.userName.valid ||
-                        !this.state.controls.password.valid}
-                  >
-                     {this.props.isLoading ? 'LOGGING IN...' : 'LOGIN'}
-                  </Btn>
+                  <View style={styles.inputContainer}>
+                     <DefaultInput
+                        placeholder='UserName' style={styles.input}
+                        value={this.state.controls.userName.value}
+                        valid={this.state.controls.userName.valid}
+                        touched={this.state.controls.userName.touched}
+                        onChangeText={(val) => this.updateInputState('userName', val)}
+                     />
+                     <DefaultInput
+                        placeholder='Password' style={styles.input}
+                        value={this.state.controls.password.value}
+                        valid={this.state.controls.password.valid}
+                        touched={this.state.controls.password.touched}
+                        onChangeText={(val) => this.updateInputState('password', val)}
+                        secureTextEntry
+                     />
+                  </View>
+
+                  <View style={styles.btnContainer}>
+                     <Btn
+                        width={200}
+                        textColor='black'
+                        color='#67B5CA'
+                        onPress={this.loginHandler}
+                        fontFamily={fonts.light}
+                        disabled={
+                           !this.state.controls.userName.valid ||
+                           !this.state.controls.password.valid}
+                     >
+                        {this.props.isLoading ? 'LOGGING IN...' : 'LOGIN'}
+                     </Btn>
+                  </View>
+                  
                </View>
-            </View>
+            </TouchableWithoutFeedback>
          </ImageBackground>
       );
    }
@@ -113,14 +117,14 @@ const styles = StyleSheet.create({
    inputContainer: {
       flex: 2,
       justifyContent: 'flex-end',
-      marginLeft: 20,
-      marginRight: 20,
+      paddingRight: 20,
+      paddingLeft: 20
    },
    btnContainer: {
       flex: 2,
       alignItems: 'center',
       justifyContent: 'flex-start',
-      marginTop: 30
+      paddingTop: 30
    },
    input: {
       backgroundColor: '#eee',
